7083ff6291f3d566ca29acffd8745dc753199370
Degr3e <glebyal2007@gmail.com>
2026-05-24 19:49:11 +0300

    Add checked multiplication

--- yalovsky.gleb/S2/math_func.cpp
+++ yalovsky.gleb/S2/math_func.cpp
@@ -61,3 +61,45 @@ yalovsky::lli_t yalovsky::sub(const lli_t& lhs, const lli_t& rhs)
   }
   return lhs - rhs;
 }
+
+yalovsky::lli_t yalovsky::mult(const lli_t& lhs, const lli_t& rhs)
+{
+  const lli_t maxValue = std::numeric_limits< lli_t >::max();
+  const lli_t minValue = std::numeric_limits< lli_t >::min();
+
+  if (lhs == 0 || rhs == 0)
+  {
+    return 0;
+  }
+  if (lhs == -1)
+  {
+    if (rhs == minValue)
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+    return -rhs;
+  }
+  if (rhs == -1)
+  {
+    if (lhs == minValue)
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+    return -lhs;
+  }
+  if (lhs > 0)
+  {
+    if ((rhs > 0 && lhs > maxValue / rhs) || (rhs < 0 && rhs < minValue / lhs))
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+  }
+  else
+  {
+    if ((rhs > 0 && lhs < minValue / rhs) || (rhs < 0 && lhs < maxValue / rhs))
+    {
+      throw std::overflow_error("Mult overflow");
+    }
+  }
+  return lhs * rhs;
+}
